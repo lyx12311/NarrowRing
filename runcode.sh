1f00365f63eeb54d3d70a96cc3a19b5db28eb597
Lucy Lu <lucy@wireless-10-104-156-128.umd.edu>
2018-09-17 16:38:03 -0400

    this shell script runs a folder generated by ICstats automatically for production runs

--- runcode.sh
+++ runcode.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-for entry in $(ls -d */):
+for entry in $(ls -d */)
 do
 	echo $entry
-	/bin/cp input.hnb $entry
-	cd $entry
+	#/bin/cp input.hnb $entry
+	/bin/cp user.in $entry
+	/bin/cp M.in $entry
+	/bin/cp stream.in $entry
+	#cd $entry
 	#/home/lyx/bin/stream2.pl stream2.in > M0.in
-	/home/lyx/bin/hndrag input.hnb
-	cd ..
+	#/home/lyx/bin/hndrag input.hnb
+	#cd -
 done
+
+hndragM *
 
